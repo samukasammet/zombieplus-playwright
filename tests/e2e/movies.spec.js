6a43c2411538a0b5b87191b3664205555c7bb72a
Samuel Franco de Godoy <samuka_sammet@hotmail.com>
2025-02-11 18:36:53 +0100

    TvShows-test-pool
    Created the test pool for tv shows

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('../support')
-
 const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
@@ -15,7 +14,14 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     const message = `O filme '${movie.title}' foi adicionado ao catálogo.`
     await page.popup.haveText(message)
 })
+test('deve poder remover um filme', async ({ page, request }) => {
+    const movie = data.to_remove
+    await request.api.postMovie(movie)
 
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.remove(movie)
+    await page.popup.haveText('Filme removido com sucesso.')
+})
 test('não deve cadastrar um titulo duplicado', async ({ page, request }) => {
     const movie = data.duplicate
     await request.api.postMovie(movie)
@@ -35,4 +41,15 @@ test('não deve cadastrar quando os campos obrigratórios não são preenchidos'
         'Campo obrigatório',
         'Campo obrigatório',
         'Campo obrigatório'])
+})
+
+test('deve buscar pelo termo zumbi', async ({ page, request }) => {
+    const movies = data.to_search
+    movies.data.forEach(async (m) => {
+        await request.api.postMovie(m)
+    })
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.search(movies.input)
+    await page.movies.tableHave(movies.outputs)
+
 })
